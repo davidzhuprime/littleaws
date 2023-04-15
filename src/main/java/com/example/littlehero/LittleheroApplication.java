12cbdeee3b9af384262abe8edcb11e62aea47b7f
davidzhuprime <davidzhuprime@yahoo.com>
2023-04-15 17:16:20 +0800

    Initial commit

---
+++ src/main/java/com/example/littlehero/LittleheroApplication.java
@@ -0,0 +1,13 @@
+package com.example.littlehero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleheroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleheroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/littlehero/LittleheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.littlehero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleheroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
